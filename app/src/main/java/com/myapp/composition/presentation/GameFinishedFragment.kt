172445e5d923746c096d65a0fcde430a2afe2651
zdorovy90@mail.ru <zdorovy90@mail.ru>
2022-02-28 18:50:56 +0300

    Implemented navigation through FragmentManager

--- app/src/main/java/com/myapp/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/myapp/composition/presentation/GameFinishedFragment.kt
@@ -4,15 +4,62 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.myapp.composition.R
+import com.myapp.composition.databinding.FragmentGameFinishedBinding
+import com.myapp.domain.entity.GameResult
 
 class GameFinishedFragment : Fragment() {
+
+    private lateinit var binding: FragmentGameFinishedBinding
+    private lateinit var gameResult: GameResult
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_game_finished, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentGameFinishedBinding.bind(view)
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                retryGame()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+    }
+
+    private fun parseArgs() {
+        gameResult = requireArguments().getSerializable(KEY_GAME_RESULT) as GameResult
+    }
+
+    private fun retryGame() {
+        requireActivity().supportFragmentManager.popBackStack(
+            GameFragment.NAME,
+            FragmentManager.POP_BACK_STACK_INCLUSIVE
+        )
+    }
+
+    companion object {
+        private const val KEY_GAME_RESULT = "game_result"
+
+        fun newInstance(gameResult: GameResult): GameFinishedFragment {
+            return GameFinishedFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_GAME_RESULT, gameResult)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/myapp/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/myapp/composition/presentation/GameFragment.kt
@@ -6,13 +6,54 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.myapp.composition.R
+import com.myapp.composition.databinding.FragmentGameBinding
+import com.myapp.domain.entity.GameResult
+import com.myapp.domain.entity.Level
 
 class GameFragment : Fragment() {
+
+    private lateinit var level: Level
+
+    private lateinit var binding: FragmentGameBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentGameBinding.bind(view)
+    }
+
+    private fun parseArgs() {
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
+            .commit()
+    }
+
+    companion object {
+
+        const val NAME = "GameFragment"
+        private const val KEY_LEVEL = "level"
+
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 }
